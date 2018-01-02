4ea62ec45e81445485524ce5318bc5cb82230800
Jakub Broz <jakub4broz@gmail.com>
2018-01-02 18:19:07 +0100

    Working!

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function buttonPressed(e, button=undefined) {
     displayValue = displayValue.substr(0, displayValue.length - 1);
   } else if (text === "=") {
     displayValue = calculate(displayValue);
+    if (displayValue !== "ERROR") {
+      displayValue = String(Math.round(displayValue * 1000000) / 1000000);
+    }
   }
 
   display.textContent = displayValue;
@@ -24,7 +27,69 @@ function buttonPressed(e, button=undefined) {
 }
 
 function calculate(string) {
-  console.log(string);
+  let operationObj = getOperationObj(string);
+  let error = false;
+  if (operationObj.numbers.length !== operationObj.operators.length + 1) {
+    error = true;
+  }
+
+  // Multiply and Divide first
+  while (operationObj.operators.indexOf(multiply) >= 0 || operationObj.operators.indexOf(divide) >= 0) {
+    operationObj.operators.forEach((operator, i) => {
+      if (operator === multiply || operator === divide) {
+        // Division by zero
+        if (operator === divide && operationObj.numbers[i + 1] == 0) {
+          error = true;
+        }
+        operationObj.numbers[i] = operate(operator, operationObj.numbers[i], operationObj.numbers[i + 1]);
+        operationObj.numbers.splice(i + 1, 1);
+        operationObj.operators.splice(i, 1);
+      }
+    });
+  }
+
+  // Then Add and Substract
+  while (operationObj.operators.length) {
+    operationObj.numbers[0] = operate(operationObj.operators[0], operationObj.numbers[0], operationObj.numbers[1]);
+    operationObj.numbers.splice(1, 1);
+    operationObj.operators.splice(0, 1);
+  }
+
+  if (error) {
+    return "ERROR";
+  }
+  return operationObj.numbers[0];
+}
+
+// Splits the String into two Arrays in operationObj
+// operationObject.numbers holds numbers in right order
+// operationObject.operators holds operators in right order
+function getOperationObj(string) {
+  string = string.split("");
+  let operationObj = string.reduce((obj, char) => {
+    if (Number.isInteger(Number(char)) || char === ".") {
+      if (obj.lastNumber) {
+        if (char === "." && obj.numbers[obj.numbers.length - 1].indexOf(".") >= 0) {
+          // Lazy to write the condition the other way
+        } else {
+          obj.numbers[obj.numbers.length - 1] += char;
+        }
+      } else {
+        obj.numbers.push(char);
+      }
+      obj.lastNumber = true;
+    } else {
+      obj.operators.push(getOperator(char));
+      obj.lastNumber = false;
+    }
+    return obj;
+  }, {
+    numbers: [],
+    operators: [],
+    lastNumber: false,
+  });
+  operationObj.numbers = operationObj.numbers.map(string => Number(string));
+  return operationObj;
 }
 
 function getOperator(operatorString) {
