af47199c763a072a54e9cb24394f136c23ba4f9e
Jakub Broz <jakub4broz@gmail.com>
2018-01-02 16:11:17 +0100

    working

--- script.js
+++ script.js
@@ -6,9 +6,61 @@ let operate = (operator, a, b) => operator(a, b);
 
 function buttonPressed(e, button=undefined) {
   button = button || e.target;
+  let text = button.textContent;
+
+  if (Number.isInteger(Number(text)) || text === ".") {
+    displayValue += button.textContent;
+  } else if (text == "+" || text == "-" || text == "*" || text == "/") {
+    operator = getOperator(text);
+    firstNumber = displayValue;
+    displayValue += ` ${text} `;
+  } else if (text === "=" && operator) {
+    secondNumber = displayValue.substring(3 + firstNumber.length);
+    if (operator === divide && !secondNumber) {
+      displayValue = "Math ERROR";
+    } else {
+      displayValue = String(Math.round(operate(operator, Number(firstNumber), Number(secondNumber)) * 1000000) / 1000000);
+    }
+    operator = null;
+    firstNumber = null;
+    secondNumber = null;
+  } else if (text === "AC") {
+    operator = null;
+    firstNumber = null;
+    secondNumber = null;
+    displayValue = "";
+  } else if (text === "DEL") {
+    if (displayValue[displayValue.length - 1] === " ") {
+      displayValue = displayValue.substr(0, displayValue.length - 3);
+      operator = null;
+      firstNumber = null;
+    } else {
+      displayValue = displayValue.substr(0, displayValue.length - 1);
+    }
+  }
+
+  display.textContent = displayValue;
   button.classList.add("clicked");
   button.addEventListener("transitionend", removeClickedClass);
 }
+
+function getOperator(operatorString) {
+  switch (operatorString) {
+    case "+":
+      return add;
+      break;
+    case "-":
+      return substract;
+      break;
+    case "*":
+      return multiply;
+      break;
+    case "/":
+      return divide;
+      break;
+  }
+}
+
 
 function removeClickedClass(e) {
   e.target.classList.remove("clicked");
@@ -26,12 +78,23 @@ function keyPressed(e) {
     buttonPressed(e, buttons.filter(button => button.textContent === "=")[0]);
   }
 
-  if (e.key === "Backspace" || e.key === "Delete") {
-    buttonPressed(e, buttons.filter(button => button.textContent === "CLEAR")[0]);
+  if (e.key === "Backspace") {
+    buttonPressed(e, buttons.filter(button => button.textContent === "DEL")[0]);
+  }
+
+  if (e.key === "Delete") {
+    buttonPressed(e, buttons.filter(button => button.textContent === "AC")[0]);
   }
 }
 
 // ********************************
+
+let displayValue = "";
+let firstNumber;
+let secondNumber;
+let operator;
+
+let display = document.querySelector(".display-text");
 
 let buttons = Array.from(document.querySelectorAll(".button"));
 buttons.forEach(button => button.addEventListener("click", buttonPressed));
